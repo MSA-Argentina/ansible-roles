120dafad28ae13b613fe330b9dbe88476e0e16a7
Franco Mariluis <fmariluis@gmail.com>
2014-07-17 14:34:16 -0300

    creating a backup before touching sources.list

--- server-bootstrap/files/configure_sources.sh
+++ server-bootstrap/files/configure_sources.sh
@@ -5,6 +5,9 @@ if [ "$UID" -ne 0 ]; then
     exit 1
 fi
 
+# Creating a backup file first
+cp /etc/apt/sources.list /etc/apt/sources.list.backup
+
 # Renaming archive.ubuntu.com, even if it's prepended by a country code (ie. ar.archive.ubuntu.com) and doing a backup
 # excluiding the backports and proposed words, which in this case we don't mirror
 sed -i.original '/backports\|proposed/! s/^deb http:\/\/.*archive.ubuntu.com/deb http:\/\/manic.msa/g' /etc/apt/sources.list

3af14675020c609bdf7c1ec209404fd3464ca54f
Franco Mariluis <fmariluis@gmail.com>
2016-04-20 15:54:01 -0300

    improve home var management

--- server-bootstrap/files/configure_sources.sh
+++ server-bootstrap/files/configure_sources.sh
@@ -10,7 +10,7 @@ cp /etc/apt/sources.list /etc/apt/sources.list.backup
 
 # Renaming archive.ubuntu.com, even if it's prepended by a country code (ie. ar.archive.ubuntu.com) and doing a backup
 # excluiding the backports and proposed words, which in this case we don't mirror
-sed -i.original '/backports\|proposed/! s/^deb http:\/\/.*archive.ubuntu.com/deb [arch=amd64] http:\/\/manic.msa/g' /etc/apt/sources.list
+sed -i.original '/backports\|proposed/! s/^deb http:\/\/.*archive.ubuntu.com/deb [arch=amd64] http:\/\/192.168.1.70/g' /etc/apt/sources.list
 
 # Renaming security.ubuntu.com
-sed -i 's/^deb http:\/\/security.ubuntu.com/deb [arch=amd64] http:\/\/manic.msa/g' /etc/apt/sources.list
+sed -i 's/^deb http:\/\/security.ubuntu.com/deb [arch=amd64] http:\/\/192.168.1.70/g' /etc/apt/sources.list
